a9c54fd22220be60b0c8ade6ea309ea59edfcbd0
John Farrell <johnjfarrell20@gmail.com>
2018-07-26 04:13:28 -0400

    Fixed directory handling in startup

--- __main__.py
+++ __main__.py
@@ -8,17 +8,15 @@ import efnote
 PATH = os.path.join(os.getenv('HOME', os.path.expanduser('~')), '.efnote/')
 
 if not os.path.isdir(PATH):
-
     print("Root directory not found!")
     generate = input("Would you like to generate it? (y/n/q) > ")
 
     if generate == "y" or generate == "yes":
         try:
             os.mkdir(PATH)
-            with open(os.path.join(PATH, "formats.config")) as out:
-                out.write("<# Journal\n<@ Date\n<@ Content")
         except:
             print("Directory Write Error!")
+            print("Attempting to write to {}".format(PATH))
             print("Check permissions maybe?")
             print("Quitting...\n")
             quit()
@@ -29,6 +27,15 @@ if not os.path.isdir(PATH):
         quit()
 
     elif generate == "q" or generate == "quit":
+        quit()
+
+if not os.path.isfile(os.path.join(PATH, "formats.config")):
+    try:
+        with open(os.path.join(PATH, "formats.config"), "w") as out:
+            out.write("<# Journal\n<@ Date\n<@ Content")
+    except:
+        print("File Write Error!")
+        print("How did this even happen?")
         quit()
 
 application = efnote.EFNote(PATH)
